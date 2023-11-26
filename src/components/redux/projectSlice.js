eff1ffab0f85eb5e72f676972f6cf69b6f03c65e
anshuman7negi <anshumannegi777@gmail.com>
2023-11-26 12:20:06 +0530

    added projects

--- src/components/redux/projectSlice.js
+++ src/components/redux/projectSlice.js
@@ -2,21 +2,32 @@ import zeromusic from '../../assets/projectImages/ziroMusic/zirodesktop.png'
 import mathMagician from '../../assets/projectImages/mathMagician/mathmagician.png'
 import demoPortfolio from '../../assets/projectImages/portfolioDemo/demoPortfolio.png'
 import flavourHub from '../../assets/projectImages/flavourHub/flavorhub.png'
+import nikeshop from '../../assets/projectImages/nikeshop/Screenshot 2023-11-25 175725.png'
+import budgetapp from '../../assets/projectImages/budgetapp/Screenshot 2023-11-26 121733.png'
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     projectData : [
       {
         id:1,
-        projectImage: mathMagician,
-        projectTitle: 'Recipe Book',
-        cardDetail: 'The Recipe Book keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe.',
+        projectImage: nikeshop,
+        projectTitle: 'Nike Shop',
+        cardDetail: 'A simple website showcasing Nike products. Built with React.js and ruby on rails. Explore, contribute, or use as a foundation for your own online store',
+        languageUsed: ['Ruby on Rails','React.js','Tailwind'],
+        live_version: 'https://nike-shop-s446.onrender.com/',
+        link_source: 'https://github.com/anshuman7negi/Nike-shop',
+      },
+      {
+        id:2,
+        projectImage: budgetapp,
+        projectTitle: 'Budget App',
+        cardDetail: 'A budgeting app is a software application that you may access from your computer, tablet or mobile device to track your finances, such as income, savings, debt payoff, or investing. Budgeting apps often will sync with your financial accounts to give you an overview in real-time when finances change.',
         languageUsed: ['Ruby on Rails','Device','Bootstrap'],
-        live_version: 'https://mathmagic-ux2q.onrender.com/quote',
-        link_source: 'https://github.com/anshuman7negi/recipe-app',
+        live_version: 'https://budget-ydgy.onrender.com/',
+        link_source: 'https://github.com/anshuman7negi/budget-app',
       },
         {
-          id:2,
+          id:3,
           projectImage: mathMagician,
           projectTitle: 'Math Magician',
           cardDetail: 'Math Magician is a single page application where users can do simple math calculations and read random math related quotes.',
@@ -25,7 +36,7 @@ const initialState = {
           link_source: 'https://github.com/anshuman7negi/math-magicians',
         },
         {
-          id:3,
+          id:4,
           projectImage: zeromusic,
           projectTitle: 'ZIRO MUSIC FESTIVAL',
           cardDetail: 'A simple application to book the ZIRO music festival tickets and get details about the music festival.In this project I have used HTML, CSS, JavaScript.',
@@ -34,7 +45,7 @@ const initialState = {
           link_source: 'https://github.com/anshuman7negi/ConcertPage',
         },
         {
-          id:4,
+          id:5,
           projectImage: flavourHub,
           projectTitle: 'Flavour Hub',
           cardDetail: "Flavour Hub is a web application that displays different meals from an API and allows users to like and comment on their favourite meals.",
@@ -43,7 +54,7 @@ const initialState = {
           link_source: 'https://github.com/anshuman7negi/Kanban_board',
         },
         {
-          id:5,
+          id:6,
           projectImage: demoPortfolio,
           projectTitle: 'Demo Portfolio',
           cardDetail: "This is my demo portfolio website to showcase my skills in HTML5, CSS3, and JAVASCRIPT.",
